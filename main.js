fa1956944dc8a7682ea75805c97bbdfbe8dc454a
HyunSang Park <jasper@teamuni.net>
2020-08-02 14:11:53 +0900

    [ADD] puppeteer을 이용한 크롤링, fs를 이용한 JSON

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
 const request = require('request');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 // Setting & Config File
 const config = require('./config/cofing.json');
+// Save Data
+const document = require('./main.json');
 
 console.log("====================================================================================");
 console.log("Checking Webhook 🔥");
 console.log(config['webhook']);
 console.log("====================================================================================");
 
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://engineering.linecorp.com/ko/blog/');
+    await page.waitFor(1000);
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
+
+    let data = {};
+
+    let temp = await page.$("#post-62382 > div > header > div > h2 > a");
+
+    data.name = await page.evaluate((data) => {
+        return data.textContent;
+    }, temp);
+
+    data.link = await page.evaluate((data) => {
+        return data.href;
+    }, temp);
+    console.log("Crawling Complete 🔥");
+    console.log(data);
+    var DataJSON = JSON.stringify(data);
+    fs.writeFileSync('main.json', DataJSON);
+    console.log("Save to another file completed 👍");
+    await browser.close();
+})();
+
 var headers = {
     'Content-type': 'application/json'
 };
 
-var dataString = '{"text":"Hello, World!"}';
+var dataString = `{"text":"${document['name']}\n ${document['link']}"}`;
 
 var options = {
     url: config['webhook'],
